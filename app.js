648be73d0db8695b47d7dbd0f758311f8977f487
Austin Papritz <austin@papritz.dev>
2022-11-14 17:11:43 -0800

    table is showing in console after disabling RLS in supabase

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 /* Imports */
 import { getFullFellowship } from './fetch-utils.js';
 /* Get DOM Elements */
-
+const listDiv = document.querySelector('#card-container');
 /* State */
 
 /* Events */
+window.addEventListener('load', async () => {
+    const fellowship = await getFullFellowship();
+    // for (let fellow of fellowship) {
+    // }
+});
 
 /* Display Functions */
 
--- fetch-utils.js
+++ fetch-utils.js
@@ -5,7 +5,6 @@ const SUPABASE_KEY =
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function getFullFellowship() {
-    const response = await client.from('Lord of the Balloons').select('*');
-    console.log(response.data);
+    const response = await client.from('balloons').select('*');
     return response;
 }
